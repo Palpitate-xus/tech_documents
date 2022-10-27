cf1bb874bcc747a648a5198804b0f79c208271e9
Palpitate-xus <xus1758011080@outlook.com>
2022-10-27 16:07:32 +0800

    push

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,16 @@ module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
     base: '/blog/',
+    themeConfig: {
+      nav: [
+          { text: '首页', link: '/' },
+          { 
+              text: 'Palpitate\'s Blog', 
+              items: [
+                  { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                  { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+              ]
+          }
+      ]
+  }
   }
