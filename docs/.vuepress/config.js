5837ce37561b0163809efd329cdf44eda72b3b02
Palpitate-xus <xus1758011080@outlook.com>
2022-11-01 17:27:13 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,21 +2,12 @@ module.exports = {
     title: 'Palpitate\'s Documents',
     description: 'Just playing around',
     base: '/tech_documents/',
-    theme: 'reco',
     themeConfig: {
-      subSidebar: 'auto', //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
-      authorAvatar: '/home.png',
-    },
-    themeConfig: {
-      nav: [
-          { text: '首页', link: '/' },
-          { text: '笔记', link: '/notes/index' },
-          { 
-              text: 'Link', 
-              items: [
-                  { text: 'Github', link: 'https://github.com/Pola-ris' },
-              ]
-          }
-      ]
-  }
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'Notes', link: '/notes/' },
+          { text: 'Github', link: 'https://github.com/Pola-ris' },
+        ]
+      },
   }
--- docs/notes/index.md
+++ docs/notes/index.md
@@ -1,1 +1,8 @@
-# test
+---
+title: Blogging with VuePress
+lang: zh-CN
+---
+
+# {{ $frontmatter.title }}
+
+My blog post is written in {{ $frontmatter.lang }}.
